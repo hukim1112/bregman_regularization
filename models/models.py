7e322fc9ca7cf36ecc5248aedfb658439804664a
dan <hyounguk1112@gmail.com>
2019-03-01 00:34:57 +0900

    update

--- models/models.py
+++ models/models.py
@@ -1,50 +1,72 @@
 
 import tensorflow as tf
 from backbone import inception
+from datasets import flower_dataset
 slim = tf.contrib.slim
 
-#param : features_columns, num_classes, learning_rate
+params = ['learning_rate', '']
 
 
-def inference(images, params, model_name = 'inception_v1'):
-    with slim.arg_scope(inception.inception_v1_arg_scope()):
-        logits, endpoints = inception.inception_v1(images, num_classes=params['num_classes'], is_training=True)
-    return logits
-def model_fn(features, labels, mode, params):
-    _features = {'images' : features}
-    images = tf.feature_column.input_layer(features=_features, feature_columns=params['feature_columns'])
-    images = tf.reshape(
-    images, shape=(-1, params['IMAGE_HEIGHT'], params['IMAGE_WIDTH'], params['IMAGE_DEPTH']))
-    #calculate logits through CNN
-    logits = inference(images, params)
-    one_hot_labels = slim.one_hot_encoding(labels, num_classes = params.num_classes)
+class model():
 
-    if mode in (tf.estimator.ModeKeys.PREDICT, tf.estimator.ModeKeys.EVAL):
-        predicted_indices = tf.argmax(input=logits, axis = 1)
-        probabilities = tf.nn.softmax(logits, name='softmax_tensor')
+    def __init__(self):
+        self.graph = tf.Graph()
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.4
+        self.sess = tf.Session(graph=self.graph, config=config)
 
-    if mode in (tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL):
+        with self.graph.as_default():
+            self.model_fn(params)
+            self.saver = tf.train.Saver()
+            self.initializer = tf.global_variables_initializer()
+
+            path_to_latest_ckpt = tf.train.latest_checkpoint(
+                checkpoint_dir=params['model_dir'])
+            if path_to_latest_ckpt == None:
+                print('scratch from random distribution')
+                self.sess.run(self.initializer)
+            else:
+                self.saver.restore(self.sess, path_to_latest_ckpt)
+                print('restored')
+
+    def train(self, params):
+
+    def eval(self, params):
+
+    def load_batch(self, dataset_dir, batch_size):
+        filepaths, class_name_to_ids = flower_dataset.load_data(dataset_dir)
+        dataset = flower_dataset.input_fn(
+            filepaths, class_name_to_ids, batch_size)
+        iterator = dataset.make_one_shot_iterator()
+        images, labels = iterator.get_next()
+        return images, labels
+
+    def inference(self, images, params):
+        with slim.arg_scope(inception.inception_v1_arg_scope()):
+            logits, endpoints = inception.inception_v1(
+                images, num_classes=params['num_classes'], is_training=True)
+        return logits
+
+    def model_fn(self, params):
+        # bring our model for training
+        images, labels = self.load_batch(
+            params['train_datadir'], params['batch_size'])
+        with tf.variable_scope("model") as self.model_scope:
+            logits = self.inference(images, params)
+
         global_step = tf.train.get_or_create_global_step()
-        label_indices = labels
-        loss = tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels, logits=logits)
+        one_hot_labels = exitf.one_hot(labels, depth=params['num_classes'])
+        loss = tf.losses.softmax_cross_entropy(onehot_labels, logits)
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=params['learning_rate'])
+        self.cross_entropy_solver = optimizer.minimize(
+            loss, global_step=global_step, var_list=self.model_scope)
         tf.summary.scalar('cross_entropy', loss)
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {
-            'class' : predicted_indices,
-            'probabilities' : probabilities
-        }
-
-        export_outputs = {'predictions' : tf.estimator.export.PredictOutput(predictions)}
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
-
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate = params['learning_rate'])
-        train_op = optimizer.minimize(loss, global_step=global_step)
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-
-    if mode == tf.estimator.ModeKeys.EVAL:
-        eval_metric_ops = {'accuracy' : tf.metrics.accuracy(label_indices, predicted_indices)}
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
+        # Reuse our model for evaluating
+        eval_images, eval_labels = self.load_batch(
+            params['eval_datadir'], params['batch_size'])
+        with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
+            eval_logits = self.inference(eval_images, params)
+        predicted_indices = tf.argmax(input=eval_logits, axis=1)
+        probabilities = tf.nn.softmax(logits, name='soft_tensor')
